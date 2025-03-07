2efb79f043d86305811768e2576a1d948f838658
Matheus Oliveira <matheus.skm@hotmail.com>
2025-03-07 16:49:52 -0300

    feat: added Pet update route

--- pets/views/PetIdView.py
+++ pets/views/PetIdView.py
@@ -2,28 +2,25 @@ import traceback
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from pets.services.create_pet_service import create_pet_service
 from pets.services.update_pet_service import update_pet_service
 from rest_framework.permissions import IsAuthenticated
-from pets.serializers.pet_serializer import PetSerializer
 from v0.errors.app_error import App_Error
 
 
-def validate_update_pet(data):
-    if "id" not in data:
-        raise App_Error("id is required", 400)
-
-
-class IndexView(APIView):
+class PetIdView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def put(self, request, pet_id):
         try:
-            pet = create_pet_service({**request.data, "tutor": request.user})
+            data_to_insert = request.data["data"]
+            query = {"id": pet_id, "tutor": request.user}
 
-            serializer = PetSerializer(pet["data"])
+            updated_pet = update_pet_service(query, data_to_insert)
 
-            return Response({"data": serializer.data}, 201)
+            if "error" in updated_pet:
+                return Response({"error": {"message": str(updated_pet["error"])}}, 500)
+
+            return Response({"data": updated_pet["data"]}, 200)
 
         except App_Error as e:
             traceback.print_exception(e)
