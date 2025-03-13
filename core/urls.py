7c0bc35539f802256f64fa4749f6755829ff92ed
Matheus Oliveira <matheus.skm@hotmail.com>
2025-03-13 16:49:05 -0300

    refactor: changed index view to class and added put method

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core.views.create_user_view import create_user_view
+from core.views.create_user_view import IndexView
 from core.views.login_view import login_view
 
 urlpatterns = [
-    path("/user", create_user_view),
+    path("/user", IndexView.as_view()),
     path("/login", login_view)
 ]
--- core/views/create_user_view.py
+++ core/views/create_user_view.py
@@ -1,34 +1,48 @@
 import traceback
 
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from core.serializers import UserSerializer
 from core.services.create_user_service import create_user_service
 from core.services.get_user_service import get_user_service
 from core.services.get_token_by_user import get_token_by_user_service
-
+from core.services.update_user_service import update_user_service
 from v0.errors.app_error import App_Error
 
 
-@api_view(["POST"])
-@permission_classes([])
-def create_user_view(request):
-    try:
-        user_already_exists = get_user_service(
-            request.data)
+class IndexView(APIView):
+    permission_classes = []
 
-        if user_already_exists["user"] is not None:
-            return Response({"error": {"message": "User already exists"}}, 405)
+    def post(self, request):
+        try:
+            user_already_exists = get_user_service(
+                request.data)
 
-        data = create_user_service(request.data)
-        token = get_token_by_user_service(data["user"])
-        user_serializer = UserSerializer(data["user"])
+            if user_already_exists["user"] is not None:
+                return Response({"error": {"message": "User already exists"}}, 405)
 
-        return Response({"token": token["data"].key, "user": user_serializer.data}, 201)
+            data = create_user_service(request.data)
+            token = get_token_by_user_service(data["user"])
+            user_serializer = UserSerializer(data["user"])
 
-    except App_Error as e:
-        traceback.print_exception(e)
-        return Response(e.toHttp(), e.status)
-    except Exception as e:
-        traceback.print_exception(e)
-        return Response({"error": {"message": str(e)}}, 500)
+            return Response({"token": token["data"].key, "user": user_serializer.data}, 201)
+
+        except App_Error as e:
+            traceback.print_exception(e)
+            return Response(e.toHttp(), e.status)
+        except Exception as e:
+            traceback.print_exception(e)
+            return Response({"error": {"message": str(e)}}, 500)
+
+    def put(self, request):
+        try:
+            update_user_service({"id": request.user.id}, request.data)
+
+            return Response(None, 200)
+
+        except App_Error as e:
+            traceback.print_exception(e)
+            return Response(e.toHttp(), e.status)
+        except Exception as e:
+            traceback.print_exception(e)
+            return Response({"error": {"message": str(e)}}, 500)
