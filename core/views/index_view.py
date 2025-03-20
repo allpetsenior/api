0defa55efd535c72c1464f0bf2b934a7bad0c49a
Matheus Oliveira <matheus.skm@hotmail.com>
2025-03-20 09:49:07 -0300

    fix: added in keyword to check request.data.id

--- core/views/index_view.py
+++ core/views/index_view.py
@@ -12,53 +12,53 @@ from v0.errors.app_error import App_Error
 
 
 class IndexView(APIView):
-  permission_classes = []
+    permission_classes = []
 
-  def post(self, request):
-    try:
-      if request.data['id'] == '':
-        request.data['id'] = None
-      user_already_exists = get_user_service(
-          request.data)
+    def post(self, request):
+        try:
+            if "id" in request.data:
+                request.data['id'] = None
+            user_already_exists = get_user_service(
+                request.data)
 
-      if user_already_exists["user"] is not None:
-        return Response({"error": {"message": "User already exists"}}, 405)
+            if user_already_exists["user"] is not None:
+                return Response({"error": {"message": "User already exists"}}, 405)
 
-      data = create_user_service(request.data)
-      token = get_token_by_user_service(data["user"])
-      user_serializer = UserSerializer(data["user"])
+            data = create_user_service(request.data)
+            token = get_token_by_user_service(data["user"])
+            user_serializer = UserSerializer(data["user"])
 
-      return Response({"token": token["data"].key, "user": user_serializer.data}, 201)
+            return Response({"token": token["data"].key, "user": user_serializer.data}, 201)
 
-    except App_Error as e:
-      traceback.print_exception(e)
-      return Response(e.toHttp(), e.status)
-    except Exception as e:
-      traceback.print_exception(e)
-      return Response({"error": {"message": str(e)}}, 500)
+        except App_Error as e:
+            traceback.print_exception(e)
+            return Response(e.toHttp(), e.status)
+        except Exception as e:
+            traceback.print_exception(e)
+            return Response({"error": {"message": str(e)}}, 500)
 
-  def put(self, request):
-    try:
-      update_user_service({"id": request.user.id}, request.data)
+    def put(self, request):
+        try:
+            update_user_service({"id": request.user.id}, request.data)
 
-      return Response(None, 200)
+            return Response(None, 200)
 
-    except App_Error as e:
-      traceback.print_exception(e)
-      return Response(e.toHttp(), e.status)
-    except Exception as e:
-      traceback.print_exception(e)
-      return Response({"error": {"message": str(e)}}, 500)
+        except App_Error as e:
+            traceback.print_exception(e)
+            return Response(e.toHttp(), e.status)
+        except Exception as e:
+            traceback.print_exception(e)
+            return Response({"error": {"message": str(e)}}, 500)
 
-  def delete(self, request):
-    try:
-      request.user.delete()
+    def delete(self, request):
+        try:
+            request.user.delete()
 
-      return Response(None, 200)
+            return Response(None, 200)
 
-    except App_Error as e:
-      traceback.print_exception(e)
-      return Response(e.toHttp(), e.status)
-    except Exception as e:
-      traceback.print_exception(e)
-      return Response({"error": {"message": str(e)}}, 500)
+        except App_Error as e:
+            traceback.print_exception(e)
+            return Response(e.toHttp(), e.status)
+        except Exception as e:
+            traceback.print_exception(e)
+            return Response({"error": {"message": str(e)}}, 500)
