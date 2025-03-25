9f62f85429184b7bb5d4c16dae448a2ae84d1e7b
Matheus Oliveira <matheus.skm@hotmail.com>
2025-03-25 16:04:26 -0300

    feat: created Recommendation model

--- ai/models.py
+++ ai/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from pets.models import Pet
+
+
+class Recommendation(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    content = models.CharField(max_length=4000)
+
+    class Type(models.TextChoices):
+        health = 'HT', _('HEALTH')
+        nutrition = 'NT', _('NUTRITION')
+        activity = 'AT', _('ACTIVITY')
+
+    type = models.CharField(
+        max_length=12,
+        choices=Type.choices,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_in = models.DateTimeField()
