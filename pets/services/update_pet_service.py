d343e6fc6b9bacaf91372c28207058435e762892
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2025-04-10 09:50:17 -0300

    bug:Correção do erro de atualização da raça srd

--- pets/services/update_pet_service.py
+++ pets/services/update_pet_service.py
@@ -1,21 +1,22 @@
 from pets.models import Pet
-from v0.errors.app_error import App_Error
 from pets.repositories.pet_race_repository import PetRaceRepository
+from v0.errors.app_error import App_Error
 
 pet_race_repo = PetRaceRepository()
 
 
 def update_pet_service(query, data):
-    try:
-        if "race" in data:
-            race = pet_race_repo.get_race({"name": data["race"]["name"]})
-            data["race"] = race
+  try:
+    if "race" in data:
+      race = pet_race_repo.get_race(
+          {"name": data["race"]["name"], 'specie': data['race']['specie']})
+      data["race"] = race
 
-        affected_rows = Pet.objects.filter(**query).update(**data)
+    affected_rows = Pet.objects.filter(**query).update(**data)
 
-        if affected_rows == 0:
-            raise App_Error("Pet not founded", 404)
+    if affected_rows == 0:
+      raise App_Error("Pet not founded", 404)
 
-        return {"data": affected_rows}
-    except Exception as e:
-        return {"error": e}
+    return {"data": affected_rows}
+  except Exception as e:
+    return {"error": e}
