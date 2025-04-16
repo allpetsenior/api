a307630c59e8b060f9bfeafcabd04604e295d099
Matheus Oliveira <matheus.skm@hotmail.com>
2025-04-16 12:56:24 -0300

    refactor: added Chatbot.get_recommendation method and recommendation_agent_id

--- ai/chatbot_provider.py
+++ ai/chatbot_provider.py
@@ -1,12 +1,13 @@
 import requests
 import json
-from app.settings import CHATBOT_API_KEY, CHATBOT_ID, CHATBOT_BASE_URL
+from app.settings import CHATBOT_API_KEY, CHATBOT_AGENT_ID, CHATBOT_BASE_URL, RECOMMENDATION_AGENT_ID
 
 
 class Chatbot():
     def __init__(self):
         api_key = CHATBOT_API_KEY
-        chatbot_id = CHATBOT_ID
+        chatbot_id = CHATBOT_AGENT_ID
+        recommendation_agent_id = RECOMMENDATION_AGENT_ID
         chatbot_base_url = CHATBOT_BASE_URL
 
         if api_key is None:
@@ -15,10 +16,33 @@ class Chatbot():
             raise Exception("CHATBOT_BASE_URL is None")
         if chatbot_id is None:
             raise Exception("CHATBOT_ID is None")
+        if recommendation_agent_id is None:
+            raise Exception("RECOMMENDATION_AGENT_ID is None")
 
         self.chatbot_id = chatbot_id
+        self.recommendation_agent_id = recommendation_agent_id
         self.api_key = api_key
         self.chatbot_base_url = chatbot_base_url
+
+    def get_recommendation(self, message):
+        headers = {
+            'Authorization': f'Bearer {self.api_key}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            "messages": message,
+            "chatbotId": self.recommendation_agent_id,
+            "stream": False,
+        }
+
+        response = requests.post(
+            self.chatbot_base_url, headers=headers, data=json.dumps(data))
+        json_data = response.json()
+
+        if response.status_code == 200:
+            return {"data": json_data['text'], "status": response.status_code}
+        else:
+            return {"error": json_data['message'], "status": response.status_code}
 
     def send_message(self, messages):
         headers = {
