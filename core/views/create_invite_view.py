686693e7ffc61d22faac8cd5e011866d22a0c90e
Renan Ferreira de Lima <renanferreiradelima98@live.com>
2025-04-19 21:47:01 -0300

    update:Melhoria no método de criar invites

--- core/views/create_invite_view.py
+++ core/views/create_invite_view.py
@@ -3,8 +3,8 @@ import traceback
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
-from core.serializers import InviteSerializer
 from core.services.create_invite_service import create_invite_service
+from core.services.get_invite_service import get_invite_service
 from v0.errors.app_error import App_Error
 
 
@@ -12,10 +12,11 @@ from v0.errors.app_error import App_Error
 @permission_classes([])
 def create_invite_view(request):
   try:
-    data = create_invite_service({**request.data, 'user': request.user})
-    invite_serializer = InviteSerializer(data['invite'])
+    if get_invite_service({'email': request.data.get('email')}).get('invite') is not None:
+      return Response({'error': {"message": "Convite já enviado"}}, 400)
 
-    return Response({"invite": invite_serializer.data}, 201)
+    create_invite_service({**request.data, 'user': request.user})
+    return Response({"message": "Obrigada pela indicação! Avisaremos ao seu contato assim que o acesso for liberado!"}, 201)
 
   except App_Error as e:
     traceback.print_exception(e)
