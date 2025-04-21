c6b465c97cdba0b7fa0ef2c716468b589c50b761
Matheus Oliveira <matheus.skm@hotmail.com>
2025-04-20 23:41:34 -0300

    feat: created get user chat

--- ai/urls.py
+++ ai/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from ai.views.prompt_pet_recommendations_view import PromptPetRecommendations
+from ai.views.get_user_chat_view import GetUserChat
 
 urlpatterns = [
     path("/prompt/pet/recommendations", PromptPetRecommendations.as_view()),
-
+    path("/chat", GetUserChat.as_view())
 ]
